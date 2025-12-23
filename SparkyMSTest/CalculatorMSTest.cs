056925c0559871e821fc3573265bf98ac8e557d1
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-12-23 21:59:25 +0200

    Add project files.

---
+++ SparkyMSTest/CalculatorMSTest.cs
@@ -0,0 +1,28 @@
+﻿using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTest
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+
+            //Act
+            int result = calculator.AddNumber(2, 3);
+
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+    }
+}
