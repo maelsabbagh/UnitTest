3052e85378061307d1dbe7a9e07518520e685113
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-12-24 12:20:26 +0200

    add customer class and NUnit customer class

---
+++ AppNUnitTest/CustomerNUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using App;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNUnitTest
+{
+    [TestFixture]
+    public class CustomerNUnitTest
+    {
+        [Test]
+        [TestCase("Mohamed","Amr")]
+        [TestCase("Ahmed", "Aly")]
+        public void GreetingAndCombine_Test(string firstName,string lastName)
+        {
+            // Arrange
+            Customer customer = new Customer();
+
+            //Act
+            string result = customer.GreetingAndCombineName(firstName, lastName);
+            // Assert
+            string expectedValue = $"Hello, {firstName} {lastName}";
+
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+    }
+}
---
+++ Calculator/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Customer
+    {
+        public string GreetingAndCombineName(string firstName,string lastName)
+        {
+            return $"Hello, {firstName} {lastName}";
+        }
+    }
+}
